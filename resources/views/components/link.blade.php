ba0204f55ba3e7fb7ca8b815c8d3a88c326b1c7b
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-02-18 09:01:50 +0700

    [FIX] Link components

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ $href }}" {{ $attributes->merge(['class' => 'inline-flex items-center transition-all duration-200 ease-in-out transform px-2 py-2 mb-2 justify-center text-xs rounded-lg group ' .  ($buttonClasses ? $buttonClasses() : '') . ' ' . ($class ?? '')]) }}>
+<a href="{{ $href }}" {{ $attributes->merge(['class' => 'inline-flex items-center transition-all duration-200 ease-in-out transform px-2 py-2 mb-2 justify-center text-xs rounded-lg group ' . $buttonClasses . ' ' . $class]) }}>
     {{ $slot }}
 </a>
